bc49adb466f65f2e6d6e5b6427db5fd44147d4a1
Viktor <staytrue.sm@yandex.ru>
2021-02-22 15:38:51 +0300

    1.1.1. Конструктор по умолчанию [#424114]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desc;
+
+    public Fix(String text) {
+        this.desc = text;
+    }
+        public static void main (String[]args){
+            String issue = "Fix me ASAP.";
+            Fix item = new Fix(issue);
+        }
+    }
