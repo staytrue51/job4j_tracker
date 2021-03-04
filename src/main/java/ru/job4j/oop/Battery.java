0db88ef96bce855a4ef61925364343d8a8f080a3
Viktor <staytrue.sm@yandex.ru>
2021-03-04 13:26:13 +0300

    1.7. Состояние объекта [#424113]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,15 +8,15 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.load += another.load;
-        another.load = 0;
+        another.load = this.load + another.load;
+        this.load = 0;
     }
 
     public static void main(String[] args) {
         Battery firstBattery = new Battery(95);
         Battery secondBattery = new Battery(10);
         System.out.println("first battery : " + firstBattery.load + ". second battery : " + secondBattery.load);
-        firstBattery.exchange(secondBattery);
+        secondBattery.exchange(firstBattery);
         System.out.println("first battery : " + firstBattery.load + ". second battery : " + secondBattery.load);
     }
 }
